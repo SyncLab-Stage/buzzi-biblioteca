d819544fb4759855a1682cb342f3415e25fca9ac
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-25 16:24:54 +0200

    feat: init Autore class with constructor

---
+++ src/Autore.java
@@ -0,0 +1,11 @@
+public class Autore {
+    private String nome;
+    private String cognome;
+    private String nazionalita;
+
+    Autore(String nome, String cognome, String nazionalita){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.nazionalita = nazionalita;
+    }
+}
