b8b6c4fc8a150e46acbdc8a9fd1a016ddc595de6
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-25 16:27:40 +0200

    feat: init Libro class

---
+++ src/Libro.java
@@ -0,0 +1,11 @@
+public class Libro {
+    private String titolo;
+    private Autore autore;
+    private int anno;
+
+    Libro(String titolo, Autore autore, int anno) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.anno = anno;
+    }
+}
