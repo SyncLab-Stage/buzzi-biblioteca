fde709ab1c8d7500555b86f65e81273c8414d53c
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-25 16:38:39 +0200

    feat: add toString to Libro for readable output

--- src/Libro.java
+++ src/Libro.java
@@ -12,4 +12,9 @@ public class Libro {
     public String getTitolo() { return titolo; }
     public Autore getAutore() { return autore; }
     public int getAnno() { return anno; }
+
+    public String toString() {
+        String libroCompleto = String.format("Titolo: %s; Autore %s; Anno: %d.", titolo, autore.getCognome(), anno);
+        return libroCompleto;
+    }
 }
