2b597d46cc62c859879a815df231fccf5ad26493
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-25 16:35:20 +0200

    feat: populate Main with sample authors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Autore autore1 = new Autore("George", "Orwell", "inglese");
+        Autore autore2 = new Autore("Luigi", "Pirandello", "italiana");
+        Autore autore3 = new Autore("Marcel", "Proust", "francese");
 
     }
 }
