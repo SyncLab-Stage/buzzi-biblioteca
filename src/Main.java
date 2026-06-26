9aac971b97e9e6d85995536f8577000ab625b394
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-26 11:46:07 +0200

    feat: add dowhile loop with printf menu and opzione input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Autore autore1 = new Autore("George", "Orwell", "inglese");
@@ -9,13 +11,21 @@ public class Main {
         Libro libro3 = new Libro("Dalla parte di Swann", autore3, 1913);
 
         Catalogo catalogo = new Catalogo();
-
         catalogo.aggiungiLibro(libro1);
         catalogo.aggiungiLibro(libro2);
         catalogo.aggiungiLibro(libro3);
 
-        System.out.println(catalogo.cercaPerAutore("Pirandello"));
-        System.out.println(catalogo.cercaPerTitolo("1984"));
-        System.out.println(catalogo.filtraPerAnno(1926));
+        Scanner scanner = new Scanner(System.in);
+        int opzione = 0;
+
+        do {
+            System.out.println("Selezionare l'opzione da eseguire");
+            System.out.printf("%-30s %d%n", "Cerca per Autore (cognome):", 1);
+            System.out.printf("%-30s %d%n", "Cerca per Titolo:", 2);
+            System.out.printf("%-30s %d%n", "Filtra per Anno:", 3);
+            System.out.printf("%-30s %d%n", "Esci:", 0);
+            System.out.print("Inserire opzione: ");
+            opzione = scanner.nextInt();
+        } while (opzione != 0);
     }
 }
