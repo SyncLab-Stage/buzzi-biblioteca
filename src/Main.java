74c843d589ffcf78533c193303c26fa9c6f696b5
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-26 11:48:11 +0200

    fix: handle exit option with empty case 0 to suppress invalid message

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,20 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Autore autore1 = new Autore("George", "Orwell", "inglese");
-        Autore autore2 = new Autore("Luigi", "Pirandello", "italiana");
-        Autore autore3 = new Autore("Marcel", "Proust", "francese");
-
-        Libro libro1 = new Libro("1984", autore1, 1949);
-        Libro libro2 = new Libro("Uno, nessuno e centomila", autore2, 1926);
-        Libro libro3 = new Libro("Dalla parte di Swann", autore3, 1913);
-
-        Catalogo catalogo = new Catalogo();
-        catalogo.aggiungiLibro(libro1);
-        catalogo.aggiungiLibro(libro2);
-        catalogo.aggiungiLibro(libro3);
-
-        Scanner scanner = new Scanner(System.in);
-        int opzione = 0;
-
-        do {
-            System.out.println("Selezionare l'opzione da eseguire");
-            System.out.printf("%-30s %d%n", "Cerca per Autore (cognome):", 1);
-            System.out.printf("%-30s %d%n", "Cerca per Titolo:", 2);
-            System.out.printf("%-30s %d%n", "Filtra per Anno:", 3);
-            System.out.printf("%-30s %d%n", "Esci:", 0);
-            System.out.print("Inserire opzione: ");
-            opzione = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (opzione) {
-                case 1 -> {
-                    System.out.print("Inserire cognome autore: ");
-                    String input = scanner.nextLine();
+switch (opzione) {
+        case 0 -> {}
+        case 1 -> {
+        System.out.print("Inserire cognome autore: ");
+String input = scanner.nextLine();
                     System.out.println(catalogo.cercaPerAutore(input));
-                }
-                case 2 -> {
-                    System.out.print("Inserire titolo: ");
-                    String input = scanner.nextLine();
+        }
+        case 2 -> {
+        System.out.print("Inserire titolo: ");
+String input = scanner.nextLine();
                     System.out.println(catalogo.cercaPerTitolo(input));
-                }
-                case 3 -> {
-                    System.out.print("Inserire anno di pubblicazione: ");
-                    int input = scanner.nextInt();
+        }
+        case 3 -> {
+        System.out.print("Inserire anno di pubblicazione: ");
+int input = scanner.nextInt();
                     scanner.nextLine();
                     System.out.println(catalogo.filtraPerAnno(input));
-                }
-                default -> System.out.println("Opzione non valida.");
+        }
+default -> System.out.println("Opzione non valida.");
             }
-        } while (opzione != 0);
-    }
-}
